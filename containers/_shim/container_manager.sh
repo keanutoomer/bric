d1cb7aa0172fbb140b9de0215eb333f51f3e56b3
keanu <keanutoomer@hotmail.com>
2025-10-31 16:36:43 +0000

    reorg

--- containers/_shim/container_manager.sh
+++ containers/_shim/container_manager.sh
@@ -1,30 +1,30 @@
 # container() - Simple container system for creating user shims to containerized applications
 container() {
     local user_home="/users/kltoomer"
-    local container_bins="${user_home}/bric/containers/apptainer/shim/bins"
-    if [ ! -d "$container_bins" ]; then
-        echo "Container bins directory not found: $container_bins"
+    local containers_dir="${user_home}/bric/containers/"
+    if [ ! -d "$containers_dir" ]; then
+        echo "Container directory not found: $containers_dir"
         return 1
     fi
 
     # Parse arguments from 'container <command> <container_name>'
     local action="$1"
     local container="$2"
-    local bin="${container_bins}/${container}_bins"
+    local bin="${containers_dir}/${container}/${container}_bins"
 
     case "$action" in
         load)
             if [ ! -d "$bin" ]; then
-                echo "Container shim not found for: $container"
+                echo "Container shim not found for ${container}"
                 # Could add fuzzy search here later; since container names are encoded with
                 # versions & bases, base matches could be listed by versison number
                 return 1
             else
                 if [[ ":$PATH:" == *":${bin}:"* ]]; then
-                    echo "Container $container already loaded"
+                    echo "Container ${container} binaries already loaded in PATH"
                 else
                     export PATH="${bin}:${PATH}"
-                    echo "Loaded: $container"
+                    echo "Loaded ${container} binaries to PATH"
                 fi
             fi
             ;;
@@ -33,27 +33,31 @@ container() {
                 export PATH="${PATH//${bin}:/}"
                 export PATH="${PATH//:${bin}/}"
                 export PATH="${PATH//${bin}/}"
-                echo "Unloaded: $container"
+                echo "Unloaded binaries for ${container} from PATH"
             else
-                echo "Container $container not loaded"
+                echo "Container ${container} not loaded in PATH"
             fi
             ;;
         list)
             echo "Checking loaded containers..."
-            for con in ~/containers/bins/*/; do
+            for con in ~/containers/*; do
                 conname=$(basename "$con")
-                if [[ ":$PATH:" == *":${con}bin:"* ]]; then
+                if [[ ":$PATH:" == *":${con}_bin:"* ]]; then
                     echo "  $conname"
                 fi
             done
             ;;
         avail)
-            echo "Available containers:"
-            ls -1 "${container_bins}" | sed 's/_bins//'
+            echo "Shimmable containers:"
+            for d in "${containers_dir}"*_bin; do
+                [ -d "$d" ] || continue
+                name=$(basename "$d")
+                name=${name%_bin}
+                echo " -- ${name}"
+            done
             ;;
         show)
-            echo "container: $container"
-            echo "Bin: $bin"
+            echo "${container} binaries"
             echo "Commands: $(ls -1 ${bin} | wc -l)"
             ;;
         *)
